5fd8443ec4c40ee5cd3861951b0bbe1ee64ed48f
aakumar07 <devopss2aws@gmail.com>
2023-09-22 14:06:02 +0000

    nx.sh

--- nx.sh
+++ nx.sh
@@ -6,10 +6,11 @@ tar -xvf latest-unix.tar.gz
 mv nexus-3/* nexus3
 chown -R ec2-user:ec2-user nexus3 sonatype-work
 cd /opt/nexus3/bin/
-echo "run_as_user="ec2-user"" >> nexus.rc
+echo run_as_user="ec2-user" >> nexus.rc
 ln -s /opt/nexus3/bin/nexus /etc/init.d/nexus
 cd /etc/init.d/
 chkconfig --add nexus
 chkconfig nexus on
 service nexus start
+systemctl start nexus
 
